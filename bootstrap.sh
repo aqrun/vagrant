f8a7b20b3480f6f8749bc711ee2dd981da1edeee
Aqrun <aqrun@oicnp.com>
2018-11-05 19:06:47 +0800

    vhost modify

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,6 @@
 
 # sudo chmod +x bootstrap.sh
 # 备份文件
-move /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
 # 链接nginx.conf
 ln -s /home/aqrun/workspace/vagrant/etc/nginx/nginx.conf /etc/nginx/nginx.conf
